98381e727bd4cbd32d9522472f959b6b4404ddd7
Pakkawat K <plugs2004@gmail.com>
2024-12-07 18:03:13 +0700

    top-main v1

--- calculator.srcs/sources_1/new/top.v
+++ calculator.srcs/sources_1/new/top.v
@@ -4,52 +4,28 @@ module top(
     input clk,              
     input btnL,            
     input btnR,            
-    input btnC,          
+    input btnC,
+    input btnU,          
+    input btnD,          
     output [6:0] seg,
     output [3:0] an,
     output dp,
     output [15:0] led
 );
+
     wire rr;
     wire [2:0] mode;
     wire [28:0] bDot;
-    wire [7:0] aDot;
+    wire [7:0] aDot;    
+    wire enCalc,enSci,enLogi,enUnit,enAge;
+    
     SegRefreshRate gen_rr(clk, rr);
-
-
+    
     assign bDot = {1'b1,28'd98778943};
     assign aDot = 8'd25;
+    main(clk,rr,btnL,btnR,btnC,seg,an,enCalc,enSci,enLogi,enUnit,enAge);
+ 
+    assign led = {11'b0,enCalc, enSci,enLogi,enUnit,enAge};
     
-    main modeSelector (
-        .clk(clk),
-        .rr(rr),
-        .btnL(btnL),
-        .btnR(btnR),
-        .btnC(btnC),
-        .seg(seg),
-        .an(an),
-        .led(led),
-        .mode(mode)
-    );
-    
-    always @(mode) begin
-        case (mode)
-            3'b001: begin 
-                displayDec(clk, btnL, btnR, bDot, aDot, seg, an, led, dp);
-            end
-            3'b010: begin 
-                displayDec(clk, btnL, btnR, bDot, aDot, seg, an, led, dp);
-            end
-            3'b011: begin
-                displayDec(clk, btnL, btnR, bDot, aDot, seg, an, led, dp);
-            end
-            3'b100: begin
-                displayDec(clk, btnL, btnR, bDot, aDot, seg, an, led, dp);
-            end
-            3'b101: begin
-                displayDec(clk, btnL, btnR, bDot, aDot, seg, an, led, dp);
-            end
-        endcase
-    end
 
 endmodule
